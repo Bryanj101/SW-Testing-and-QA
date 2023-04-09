7b511e012c2ece86aecb9685ef1f8f9ab9cfa08a
Bryan <btj117@msstate.edu>
2023-04-09 02:16:05 -0500

    django tests

--- BMI/tests.py
+++ BMI/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class PageTest(TestCase):
+    def test_page_landing(self):
+        c = Client()
+        response = c.get('')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_page_post(self):
+        c = Client()
+        response = c.post('')
+        self.assertEqual(response.status_code, 200)
